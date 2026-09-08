0153363784d217b04e2364fe7b046c60c8fd4d8a
Chand-Dev-ui <rohaanchand.business@gmail.com>
2026-09-08 19:00:17 +0500

    Update Supabase anon key in config.js

--- assets/ config.js
+++ assets/ config.js
@@ -2,7 +2,7 @@
 window.SEAWAIS_CONFIG = {
   SUPABASE_URL: "https://ktspvzvkphqqpluczlew.supabase.co",
 
-  SUPABASE_ANON_KEY: "YOUR_EXISTING_SUPABASE_ANON_KEY",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0c3B2enZrcGhxcXBsdWN6bGV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3Nzk4OTAsImV4cCI6MjEwNDM1NTg5MH0.1bPfazANl2Kdyyi4dCH4_l63i8ec-HiPbUEuDNzhM3g",
 
   BANK_DETAILS: {
     bank_name: "Easypaisa",
